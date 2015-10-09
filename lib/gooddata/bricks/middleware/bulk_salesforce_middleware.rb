d9956d0aae38c5b6a2ac308f4c1bc72d11ead8f1
Zulfiqar Ali <dev+zulfiqar@influitive.com>
2015-10-09 10:43:11 -0400

    remove unused dependencies

--- lib/gooddata/bricks/middleware/bulk_salesforce_middleware.rb
+++ lib/gooddata/bricks/middleware/bulk_salesforce_middleware.rb
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-require 'salesforce_bulk_query'
+# require 'salesforce_bulk_query'
 
 require_relative 'base_middleware'
 
--- lib/gooddata/rest/connection.rb
+++ lib/gooddata/rest/connection.rb
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-require 'terminal-table'
+# require 'terminal-table'
 require 'securerandom'
 require 'monitor'
 require 'thread_safe'
